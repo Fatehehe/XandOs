1ed4c8060297ca7b7d3569ed92cd61f4bf7c96d2
Fatih <fatakhillah.khaqo@gmail.com>
2024-07-20 15:48:36 +0700

    Initial Commit

---
+++ XandOs/XandOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  XandOsApp.swift
+//  XandOs
+//
+//  Created by Fatakhillah Khaqo on 20/07/24.
+//
+
+import SwiftUI
+
+@main
+struct XandOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
