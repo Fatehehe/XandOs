7c2824e2e06d4dae822679f1de95b6db39047b7b
Fatih <fatakhillah.khaqo@gmail.com>
2024-07-21 11:31:40 +0700

    multipeer commit

---
+++ XandOs/MultiPeerConnectivity/MPGameMove.swift
@@ -0,0 +1,22 @@
+//
+//  MPGameMove.swift
+//  XandOs
+//
+//  Created by Fatakhillah Khaqo on 21/07/24.
+//
+
+import Foundation
+
+struct MPGameMove: Codable {
+    enum Action: Int, Codable{
+        case start, move, reset, end
+    }
+    
+    let action: Action
+    let playerName: String?
+    let index: Int?
+    
+    func data() -> Data? {
+        try? JSONEncoder().encode(self)
+    }
+}
